a953d3044bae924cd2e618ed2dce8c0439b8a069
sanga <tkddk9141@naver.com>
2025-06-27 18:18:42 +0900

    feat. 유효성 검증 추가

--- src/main/java/com/project/emologue/exception/GlobalExceptionHandler.java
+++ src/main/java/com/project/emologue/exception/GlobalExceptionHandler.java
@@ -1,7 +1,10 @@
 package com.project.emologue.exception;
 
 import com.project.emologue.model.error.ErrorResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -14,6 +17,22 @@ public class GlobalExceptionHandler {
             new ErrorResponse(e.getStatus(), e.getMessage()),
                 e.getStatus()
         );
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handlerMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        var errorMessage = e.getFieldErrors().stream()
+                .map(fieldError -> (fieldError.getField() + ": " + fieldError.getDefaultMessage()))
+                .toList().toString();
+
+        return new ResponseEntity<>(new ErrorResponse(HttpStatus.BAD_REQUEST, errorMessage),
+                HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> handlerHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+        return new ResponseEntity<>(new ErrorResponse(HttpStatus.BAD_REQUEST, "Required request body is missing"),
+                HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(RuntimeException.class)
