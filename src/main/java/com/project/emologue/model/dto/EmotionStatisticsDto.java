0cde35d2cf8e7965768ab6629999c58b4ab1e01b
sanga <tkddk9141@naver.com>
2025-07-28 18:14:00 +0900

    refactor. remove setter (DTO)

--- src/main/java/com/project/emologue/model/dto/EmotionStatisticsDto.java
+++ src/main/java/com/project/emologue/model/dto/EmotionStatisticsDto.java
@@ -1,7 +1,5 @@
 package com.project.emologue.model.dto;
 
-import java.util.Objects;
-
 public class EmotionStatisticsDto {
     private String emotionType;
     private long count;
@@ -17,36 +15,12 @@ public class EmotionStatisticsDto {
         return emotionType;
     }
 
-    public void setEmotionType(String emotionType) {
-        this.emotionType = emotionType;
-    }
-
     public long getCount() {
         return count;
-    }
-
-    public void setCount(long count) {
-        this.count = count;
     }
 
     public double getRatio() {
         return ratio;
     }
 
-    public void setRatio(double ratio) {
-        this.ratio = ratio;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        EmotionStatisticsDto that = (EmotionStatisticsDto) o;
-        return count == that.count && Double.compare(ratio, that.ratio) == 0 && emotionType == that.emotionType;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(emotionType, count, ratio);
-    }
 }
